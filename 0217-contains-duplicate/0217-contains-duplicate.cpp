3da40c1adce78b3018a40bbfc8610ca15bce95d6
vinaytheprogrammer <49182318+vinaytheprogrammer@users.noreply.github.com>
2023-11-19 17:03:32 +0530

    Time: 146 ms (9.33%), Space: 73.8 MB (6.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       set<int> s(nums.begin(),nums.end());
+       set<int> s;
         for(int i=0;i<nums.size();i++)
         {
             s.insert(nums[i]);
