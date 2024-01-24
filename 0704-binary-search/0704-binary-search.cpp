10e52efb17520bd070cc909fb9f4338b8bc627e2
vinaytheprogrammer <49182318+vinaytheprogrammer@users.noreply.github.com>
2024-01-24 20:16:21 +0530

    Time: 36 ms (18.03%), Space: 29.9 MB (5.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,27 +1,27 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size()-1;
-        
-        int mid = start + (end-start)/2;
-        
-        while(start<=end)
-        {
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(nums[mid]<target)
-            {
-                start = mid +1;
-            }
-            else
-            {
-                end = mid-1;
-            }
-            mid = start + (end-start)/2;
-        }
+    private: 
+    int solve(vector<int> &nums,int s,int e,int target){
+    if(s>e){
         return -1;
+    }
+
+    
+    int mid = (s+e)/2;
+    // cout<<s<<" "<<e<<endl;
+    if(nums[mid] == target){
+        return mid;
+    }
+
+    
+    if(nums[mid]>target){
+        return solve(nums,s,mid-1,target);
+    }
+    else
+        return solve(nums,mid+1,e,target);
+    }
+    public:
+    
+    int search(vector<int>& nums, int target) {
+        return solve(nums,0,nums.size()-1,target);    
     }
 };
