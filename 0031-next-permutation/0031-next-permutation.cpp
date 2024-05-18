0de86423ec9edd3fc2a7e3a5fd455edf53ae628d
vinaygupta.nitjsr <49182318+vinaytheprogrammer@users.noreply.github.com>
2024-05-18 19:09:51 +0530

    Time: 7 ms (15.48%), Space: 16.4 MB (14.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -17,7 +17,7 @@ public:
         
         // ---
         
-        // rest of the work
+        // rest of the cases
         
         vector<int> vec;
         int trace;
@@ -31,15 +31,12 @@ public:
                     vec.push_back(nums[it]);
                 }
                 
-                trace = i - 1;
+                trace = i - 1; // mark this location as trace
                 break;
             }
         }
         
-        for(auto val : vec){
-            cout<<val<<" ";
-        }
-        
+        // for getting the next greater number of the trace
         sort(vec.begin(), vec.end());
         int get;
         for(int i = 0 ;i < vec.size() - 1;i++){
@@ -48,7 +45,8 @@ public:
                 break;
             }
         }
-
+        
+        //delete that get value from the temporary vector
         int temp = nums[trace];
         auto it = find(vec.begin(), vec.end(), get);
         
@@ -56,9 +54,11 @@ public:
             vec.erase(it);
         }
        
+        //push that get value in nums
         sort(vec.begin(),vec.end());
         nums[trace] = get;
         
+        //update nums with sorted value after the get value(trace location)
         int k = 0;
         for(int j = trace+1; j < nums.size(); j++){
             nums[j] = vec[k];k++; 
