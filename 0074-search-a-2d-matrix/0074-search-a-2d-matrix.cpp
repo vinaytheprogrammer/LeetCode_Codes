8bd7e374437c10354ad71516b4f6937ec70f4370
vinaygupta.nitjsr <49182318+vinaytheprogrammer@users.noreply.github.com>
2024-05-29 17:21:31 +0530

    Time: 3 ms (66.69%), Space: 12 MB (48.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,21 +4,25 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         
-        int s = 0, e = rows * cols - 1;
+        int start = 0;
+        int end = rows*cols -1;
         
-        while(s <= e)
-        {
-            int mid = s + (e - s)/2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
             int row = mid / cols;
             int col = mid % cols;
-            
-            if(matrix[row][col] == target)
+            if(matrix[row][col] == target){
                 return 1;
-            else if(matrix[row][col] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
+            }
+            else if(matrix[row][col] < target){
+                start = mid +1;
+            }
+            else{
+                end = mid - 1;
+            }
         }
+        
         return 0;
     }
 };
