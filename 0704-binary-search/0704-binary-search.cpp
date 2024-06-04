a6c427de4e1f7b93c90e46007edae22aac19d37c
vinaygupta.nitjsr <49182318+vinaytheprogrammer@users.noreply.github.com>
2024-06-04 08:44:48 +0530

    Time: 36 ms (5.89%), Space: 29.9 MB (66.27%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,27 +1,22 @@
 class Solution {
     private: 
-    int solve(vector<int> &nums,int s,int e,int target){
-    if(s>e){
-        return -1;
-    }
-
     
-    int mid = (s+e)/2;
-    // cout<<s<<" "<<e<<endl;
-    if(nums[mid] == target){
-        return mid;
-    }
-
-    
-    if(nums[mid]>target){
-        return solve(nums,s,mid-1,target);
-    }
-    else
-        return solve(nums,mid+1,e,target);
-    }
     public:
     
     int search(vector<int>& nums, int target) {
-        return solve(nums,0,nums.size()-1,target);    
+          int s =0, e = nums.size() -1, mid = s + (e -s)/2;
+        
+        while(s <= e){
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                s = mid +1;
+            else
+                e = mid -1;
+            
+            mid = s + (e-s)/2;
+        }
+        
+        return -1;
     }
 };
